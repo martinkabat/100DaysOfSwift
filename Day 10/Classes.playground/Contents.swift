33dbfd416381027219794161fdb4b9e49491b86a
Martin Kabát <martin.kabat@styrax.cz>
2022-02-09 00:29:21 +0100

    Day 10 of 100.

--- Day 10/Classes.playground/Contents.swift
+++ Day 10/Classes.playground/Contents.swift
@@ -47,6 +47,55 @@ alik.zastekej()
 // (4) Final classes
 // Disallowing to inherit from this class
 final class Civava {
-    
 }
 
+
+// (5) Copying objects
+class Singer {
+    var name = "Taylor Swift"
+}
+
+var singer = Singer()
+print(singer.name)
+
+var singerCopy = singer
+singerCopy.name = "Justin Bieber"
+
+print(singer.name)
+
+// By changing property value of class instance copy it changes the value of the original property as well.
+// If the Singer class is struct rather than class, the singer variable name would stay Taylor Swift.
+
+
+// (6) Deinitializers
+// deinit is called once the last instance of the class is destroyed.
+class Person {
+    var name = "John Doe"
+    
+    init(){
+        print("\(name) is alive!")
+    }
+    
+    func printGreeting() {
+        print("Hello, I'm \(name)")
+    }
+    
+    deinit {
+        print("\(name) is no more.")
+    }
+}
+
+for _ in 1...3 {
+    let person = Person()
+    person.printGreeting()
+}
+
+
+// (7) Mutability
+class Zpevak {
+    var jmeno = "Krejci Svist" // It is possible to forbid properties of the class to change simply by changing property type from variable to constant.
+}
+
+let krejci = Zpevak()
+krejci.jmeno = "Eda Silenej"
+print(krejci.jmeno)
