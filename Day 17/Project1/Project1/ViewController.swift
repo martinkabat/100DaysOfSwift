bf858d8969057f88c5342ef2b9ad8c163ef97b8c
Martin Kabát <martin.kabat@styrax.cz>
2022-03-22 12:51:03 +0100

    Day 17 of 100. Second part of the first project.

---
+++ Day 17/Project1/Project1/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Martin Kabát on 21.03.2022.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    var pictures = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Storm Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let fm = FileManager.default // load file manager
+        let path = Bundle.main.resourcePath! // force unwrap main resource path of the main bundle (it always exists)
+        let items = try! fm.contentsOfDirectory(atPath: path) // get all the items from this paths
+        
+        // loop through the items keeping only the ones with the "nssl" prefix
+        for item in items {
+            if item.hasPrefix("nssl") {
+                // this is the picture to load!
+                pictures.append(item)
+            }
+        }
+        print(pictures) // show me the name of the pictures
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.selectedImage = pictures[indexPath.row]
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+}
+
