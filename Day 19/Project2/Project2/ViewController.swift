354251ba6cc2a61f1238d9b0a31712589fec5513
Martin Kabát <martin.kabat@styrax.cz>
2022-03-25 15:14:06 +0100

    Day 19 of 100. Second project starts. Or in the Big Bang Theory terminology: Paul Hudson presents: Fun with Flags!

---
+++ Day 19/Project2/Project2/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Martin Kabát on 25.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+    var countries = [String]()
+    var score = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        countries.append(contentsOf: "estonia")
+//        countries.append(contentsOf: "france")
+        
+        countries += ["estonia", "france", "germany",
+                      "ireland", "italy", "monaco",
+                      "nigeria", "poland", "russia",
+                      "spain", "uk", "us"]
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+        
+        askQuestion()
+    }
+    
+    func askQuestion() {
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+    }
+
+}
+
