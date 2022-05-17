95fdfbb9d141b684ac52c1ac631a2f117ac9e1b8
Martin Kabát <martin.kabat@styrax.cz>
2022-05-17 13:16:21 +0200

    Day 27 of 100. Project 5 starts.

---
+++ Day 27/Project5/Project5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project5
+//
+//  Created by Martin Kabát on 17.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
