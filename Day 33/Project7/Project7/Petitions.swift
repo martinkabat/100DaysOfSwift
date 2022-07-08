28d30edf4dcdcf4741641538e7200d243d4eeddf
Martin Kabát <martin.kabat@styrax.cz>
2022-07-08 17:28:38 +0200

    Day 33 and 34 of 100 Days of Swift. Codable, tab bars and a little bit of HTML.

---
+++ Day 33/Project7/Project7/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by Martin Kabát on 07.07.2022.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
---
+++ Day 34/Project7/Project7/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by Martin Kabát on 07.07.2022.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
