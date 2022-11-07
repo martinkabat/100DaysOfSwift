236d08c4e868823aeeee69784b63fb8441e89da9
Martin Kabát <martin.kabat@styrax.cz>
2022-11-07 12:59:23 +0100

    Day 56 of 100. Project 14 finished.

---
+++ Day 56/Project14/Project14/GameScene.swift
@@ -0,0 +1,127 @@
+//
+//  GameScene.swift
+//  Project14
+//
+//  Created by Martin Kabát on 04.11.2022.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    var slots = [WhackSlot]()
+    var gameScore: SKLabelNode!
+    var score = 0 {
+        didSet {
+            gameScore.text = "Score \(score )"
+        }
+    }
+    
+    var popupTime = 0.85
+    var numRounds = 0
+    
+    override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "whackBackground")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
+        
+        gameScore = SKLabelNode(fontNamed: "Chalkduster")
+        gameScore.text = "Score: 0"
+        gameScore.position = CGPoint(x: 8, y: 8)
+        gameScore.horizontalAlignmentMode = .left
+        gameScore.fontSize = 48
+        addChild(gameScore)
+        
+        for i in 0..<5 { createSlot(at: CGPoint(x: 100 + (i * 170), y: 410))}
+        for i in 0..<4 { createSlot(at: CGPoint(x: 100 + (i * 170), y: 320))}
+        for i in 0..<5 { createSlot(at: CGPoint(x: 100 + (i * 170), y: 230))}
+        for i in 0..<4 { createSlot(at: CGPoint(x: 100 + (i * 170), y: 140))}
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) { [weak self] in
+            self?.createEnemy()
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        let tapNodes = nodes(at: location)
+        for node in tapNodes {
+            guard let whackSlot = node.parent?.parent as? WhackSlot else { continue }
+            
+            if !whackSlot.isVisible { continue }
+            if whackSlot.isHit { continue }
+            whackSlot.hit()
+            
+            if let smoke = SKEmitterNode(fileNamed: "Smoke.sks") {
+                smoke.position = location
+                addChild(smoke)
+            }
+            
+            if node.name == "charFriend" {
+                // this should not be whacked
+                score -= 5
+                
+                run(SKAction.playSoundFileNamed("whackBad.caf", waitForCompletion: false))
+            } else if node.name == "charEnemy" {
+                // this should be whacked
+                whackSlot.charNode.xScale = 0.85
+                whackSlot.charNode.yScale = 0.85
+                score += 1
+                
+                run(SKAction.playSoundFileNamed("whack.caf", waitForCompletion: false))
+            }
+        }
+    }
+    
+    func createSlot(at postion: CGPoint) {
+        let slot = WhackSlot()
+        slot.configure(at: postion)
+        addChild(slot)
+        slots.append(slot)
+    }
+    
+    func createEnemy() {
+        numRounds += 1
+        
+        if numRounds >= 30 {
+            for slot in slots {
+                slot.hide()
+            }
+            
+            let gameOver = SKSpriteNode(imageNamed: "gameOver")
+            gameOver.position = CGPoint(x: 512, y: 384)
+            gameOver.zPosition = 1
+            addChild(gameOver)
+            
+            let gameOverScore = SKLabelNode(text: "Final score: \(score)")
+            gameOverScore.fontSize = 50
+            gameOverScore.position = CGPoint(x: 512, y: 300)
+            gameOverScore.zPosition = 1
+            addChild(gameOverScore)
+            
+            run(SKAction.playSoundFileNamed("myGameOver.m4a", waitForCompletion: false))
+            
+            return
+        }
+        
+        popupTime *= 0.991
+        
+        slots.shuffle()
+        slots[0].show(hideTime: popupTime)
+        
+        if Int.random(in: 0...12) > 4 { slots[1].show(hideTime: popupTime) }
+        if Int.random(in: 0...12) > 8 { slots[2].show(hideTime: popupTime) }
+        if Int.random(in: 0...12) > 10 { slots[3].show(hideTime: popupTime) }
+        if Int.random(in: 0...12) > 11 { slots[4].show(hideTime: popupTime) }
+        
+        let minDelay = popupTime / 2.0
+        let maxDelay = popupTime * 2.0
+        let delay =  Double.random(in: minDelay...maxDelay)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + delay) { [weak self] in
+            self?.createEnemy()
+        }
+    }
+}
