d161343e92aa30ce552117383a487c853649e8c1
Martin Kabát <martin.kabat@styrax.cz>
2022-11-08 08:39:26 +0100

    Day 57 of 100. Animations.

---
+++ Day 57/Project15/Project15/ViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  Project15
+//
+//  Created by Martin Kabát on 08.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var imageView: UIImageView!
+    var currentAnimation = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imageView = UIImageView(image: UIImage(named: "penguin"))
+        imageView.center = CGPoint(x: view.center.x, y: view.center.y)
+        view.addSubview(imageView)
+    }
+
+
+    @IBAction func tapped(_ sender: UIButton) {
+        sender.isHidden = true
+        
+//        UIView.animate(withDuration: 1, delay: 0, options: [], animations: {
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 5, options: [], animations: {
+            switch self.currentAnimation {
+            case 0:
+                self.imageView.transform = CGAffineTransform(scaleX: 2, y: 2)
+                break
+            case 1:
+                self.imageView.transform = .identity
+            case 2:
+                self.imageView.transform = CGAffineTransform(translationX: -256, y: -256)
+            case 3:
+                self.imageView.transform = .identity
+            case 4:
+                self.imageView.transform = CGAffineTransform(rotationAngle: .pi)
+            case 5:
+                self.imageView.transform = .identity
+            case 6:
+                self.imageView.alpha = 0.1
+                self.imageView.backgroundColor = .green
+            case 7:
+                self.imageView.alpha = 1
+                self.imageView.backgroundColor = .clear
+                default:
+                    break
+            }
+        }) { finished in
+            sender.isHidden = false
+        }
+        
+        currentAnimation += 1
+        
+        if currentAnimation > 7 {
+            currentAnimation = 0
+        }
+    }
+}
+
